c4e18b716e3ffbca925b8e4a91c0cea5dd4b8902
Oleksandra Chornous <44673760+AlexUaNo@users.noreply.github.com>
2024-03-22 15:52:03 +0100

    Add files via upload

--- webserver.py
+++ webserver.py
@@ -10,38 +10,30 @@ import sys                          # In order to terminate the program
 
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-# Prepare a server socket
-serverPort = 8080                   
+serverPort = 8080                                               # Prepare a server socket       
 serverSocket.bind(('127.0.0.1', serverPort))
 serverSocket.listen(1)                                          # A server socket will listen the port 8080
 
 while True:                                                 	# Establish the connection 
     print('Ready to serve...') 
-    connectionSocket, addr = serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept()              # Server waits on accept() for onnections
 
     try:                                                    # To encapsulate the code responsible for handling the HTTP request, reading the requested file, and sending the response.
-        message = connectionSocket.recv(1024).decode()
+        message = connectionSocket.recv(1024).decode()      # Read data from the client and print 
         filename = message.split()[1]
         f = open(filename[1:])
+        ######################## to check!
         outputdata = f.read() 
         connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())        # Send one HTTP header line into socket
-                                        
-        # The for loop from the teacher's file (I mean - for i in range(0, len(outputdata)):...) should be placed within the try block. 
-        # Otherwise, it will run (and send the content of the requested file to the client) even if an exception occurs.
-        # In addition, we try to iterate over the length of outputdata there, but outputdata is a string, so it doesn't need to be indexed. 
-        # We can directly send outputdata without iterating over its characters.
         
-        connectionSocket.send(outputdata[i].encode())                   # Send the content of the requested file to the client        
-        #connectionSocket.send("\r\n".encode())  
-        connectionSocket.close()                                        # The connection is closed at the end.
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())           # Send the content of the requested file to the client 
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()                                # The connection is closed at the end.
     
     except IOError:             # In case of error (e.g., the file not being found), the server sends a "404 Not Found" message to the client.                             
         connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\nFile Not Found".encode())
-        connectionSocket.close()                    # The connection/client socket is closed if an exception occurs.
+        connectionSocket.close()                    # The client socket is closed if an exception occurs.
 
 serverSocket.close()                        # To close the connection after the loop: to be sure that all data is sent before closing the connection
 sys.exit()                                  # Terminate the program after sending the data
-
-
-
-
